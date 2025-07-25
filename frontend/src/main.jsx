c33e88cffba0382934c9177f3a275022558d5501
kahkashan <kahkashanjaween786@gmail.com>
2025-07-25 19:27:12 +0530

    update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import { AuthProvider } from './context/authContext.jsx'
+import { AuthProvider } from './context/AuthContext.jsx'
 import { CartProvider } from './context/CartContext.jsx'
 import MainLayout from './layout/MainLayout.jsx'
 
